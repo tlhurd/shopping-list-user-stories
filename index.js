11fa292e2ef543792c6aaa263cff3f698edcd383
tlhurd <tigerlillyhurd@gmail.com>
2020-11-17 11:50:44 -0500

    added store

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 'use strict';
+// `STORE` is responsible for storing the underlying data
+//that our app needs to keep track of in order to work.
+//
+// for a shopping list, our data model is pretty simple.
+// we just have an array of shopping list items. each one
+//is an object with a `name` and a `checked` property that
+//indicates if it's checked off or not.
+//we're pre-adding items to the shopping list so there's
+// something to see when the page first loads.
+const STORE = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
+];
+
+
+
 function renderShoppingList() {
   // this function will be responsible for rendering the shopping list in
   // the DOM
   console.log('`renderShoppingList` ran');
+  const shoppingListItemsString =
+  '<li>apples</li>';
 }
   
   
@@ -30,5 +50,12 @@ function handleDeleteItemClicked() {
 // that handle new item submission and user clicks on the "check" and "delete" buttons
 // for individual shopping list items.
 function handleShoppingList() {
-  
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+
 }
+
+//when the page loads, call `handleShoppingList`
+$(handleShoppingList);
