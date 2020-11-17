e3ce5b13990c943a710f52c72b96fd488383142b
tlhurd <tigerlillyhurd@gmail.com>
2020-11-17 11:38:13 -0500

    js setup

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+function renderShoppingList() {
+  // this function will be responsible for rendering the shopping list in
+  // the DOM
+  console.log('`renderShoppingList` ran');
+}
+  
+  
+function handleNewItemSubmit() {
+  // this function will be responsible for when users add a new shopping list item
+  console.log('`handleNewItemSubmit` ran');
+}
+  
+  
+function handleItemCheckClicked() {
+  // this function will be responsible for when users click the "check" button on
+  // a shopping list item.
+  console.log('`handleItemCheckClicked` ran');
+}
+  
+  
+function handleDeleteItemClicked() {
+// this function will be responsible for when users want to delete a shopping list
+// item
+  console.log('`handleDeleteItemClicked` ran');
+}
+  
+// this function will be our callback when the page loads. it's responsible for
+// initially rendering the shopping list, and activating our individual functions
+// that handle new item submission and user clicks on the "check" and "delete" buttons
+// for individual shopping list items.
+function handleShoppingList() {
+  
+}
